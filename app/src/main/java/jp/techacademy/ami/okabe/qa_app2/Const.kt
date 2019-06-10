f1070e093d0b4414a01f5cf295af6e65c9c27ce0
amiokabe <amuntori@gmail.com>
2019-06-10 10:42:19 +0200

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ami/okabe/qa_app2/Const.kt
@@ -0,0 +1,7 @@
+package jp.techacademy.ami.okabe.qa_app2
+
+const val UsersPATH = "users"
+const val ContentsPATH = "contents"
+const val AnswersPATH = "answers"
+const val NameKEY = "name"
+const val FavouritesPATH = "favourites"
